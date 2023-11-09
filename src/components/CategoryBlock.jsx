c4f51524cce16681bba836f4e6d6c2e0da63a87c
Vadim Sokolov <108888585+vadimcodes@users.noreply.github.com>
2023-11-09 18:20:33 +0300

    инициализация проекта

---
+++ src/components/CategoryBlock.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import CategoryBlockLink from './CategoryLink';
+
+function CategoryBlock() {
+  // пустой массив для хранения списка категорий
+  const [items, setItems] = React.useState([]);
+
+  // получение JSON со всеми объектами и передача данных в массив setCategory
+  React.useEffect(() => {
+    axios.get(`https://65304cd26c756603295e855d.mockapi.io/items?`).then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+
+  // React.useEffect(() => {
+  //   fetch(`https://api.sbis.ru/retail/nomenclature/list?pointId=306&priceListId=13`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'X-SBISAccessToken':
+  //         'WFZzNS0uUjoSVVwMiRbKENGXlZIZy9AWkIxKVg2RXNiaTBLcH5oPVRCeWNHSFM2QzAsXXZoUT5UbDJIaSQoMzIwMjMtMTEtMDggMTI6NTY6NTguMjE0MDE3',
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((res) => console.log(res));
+  // }, []);
+
+  return (
+    <section className="categories">
+      <div className="container">
+        <div className="categories__up">
+          <h2 className="categories__title title">Категории товаров</h2>
+          <p className="categories__descr descr">
+            Мы предлагаем широкий ассортимент товаров для зимнего спорта и активного отдыха. В нашем
+            магазине вы найдете все, что нужно для покорения снежных склонов, ледяных катков
+            и морозных просторов
+          </p>
+        </div>
+        <ul className="categories__list list-reset grid">
+          {items.map((obj, i) => (
+            <CategoryBlockLink key={i} hierarchicalParent={obj.hierarchicalParent} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default CategoryBlock;
